2ca8cbd439a6557fb7cf43623d749c5632cc5b05
Greg Sweet <greg@ccdzine.com>
2018-06-17 12:18:38 -0800

    Begin settings sections

--- admin/class-settings.php
+++ admin/class-settings.php
@@ -33,7 +33,82 @@ class Controlled_Chaos_Block_Settings {
 	 */
 	public function __construct() {
 
+		// Register settings.
+		add_action( 'admin_init', [ $this, 'settings' ] );
 
+	}
+
+	/**
+	 * Register settings via the WordPress Settings API.
+	 * 
+	 * @since  1.0.0
+	 * @return mixed
+	 */
+	public function settings() {
+
+		/**
+		 * Block usage options.
+		 */
+		add_settings_section( 'ccb-block-usage', __( 'Block Usage', 'controlled-chaos-blocks' ), [ $this, 'block_usage_section_callback' ], 'ccb-block-usage' );
+
+		// Disable blocks globally.
+		add_settings_field( 'ccb_disable_globally', __( 'Disable globally', 'controlled-chaos-blocks' ), [ $this, 'ccb_disable_globally_callback' ], 'ccb-block-usage', 'ccb-block-usage', [ esc_html__( 'Disable editor blocks for all post types.', 'controlled-chaos-blocks' ) ] );
+
+		register_setting(
+			'ccb-block-usage',
+			'ccb_disable_globally'
+		);
+
+		/**
+		 * Default blocks options.
+		 */
+		add_settings_section( 'ccb-default-blocks', __( 'Default Blocks', 'controlled-chaos-blocks' ), [ $this, 'default_blocks_section_callback' ], 'ccb-default-blocks' );
+
+	}
+
+	/**
+	 * Block Usage section callback.
+	 * 
+	 * @since  1.0.0
+	 * @return string
+	 */
+	public function block_usage_section_callback( $args ) {
+
+		$html = sprintf( '<p>%1s</p>', esc_html__( 'You can choose to disable WordPress editor blocks and use the classic editor.' ) );
+
+		echo $html;
+
+	}
+
+	/**
+	 * Default Blocks section callback.
+	 * 
+	 * @since  1.0.0
+	 * @return string
+	 */
+	public function default_blocks_section_callback( $args ) {
+
+		$html = sprintf( '<p>%1s</p>', esc_html__( 'You can choose to individually disable default WordPress editor blocks.' ) );
+
+		echo $html;
+
+	}
+
+	/**
+	 * Disable blocks globally.
+	 * 
+	 * @since  1.0.0
+	 * @return mixed
+	 */
+	public function ccb_disable_globally_callback( $args ) {
+
+		$option = get_option( 'ccb_disable_globally' );
+
+		$html = '<p><input type="checkbox" id="ccb_disable_globally" name="ccb_disable_globally" value="1" ' . checked( 1, $option, false ) . '/>';
+		
+		$html .= '<label for="ccb_disable_globally"> '  . $args[0] . '</label></p>';
+
+		echo $html;
 
 	}
 
--- admin/partials/admin-page.php
+++ admin/partials/admin-page.php
@@ -14,14 +14,27 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-$active_tab = 'block-use';
+$active_tab = 'block-usage';
 if ( isset( $_GET[ 'tab' ] ) ) {
-    $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'block-use';
+    $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'block-usage';
 } ?>
 <div class="wrap">
 	<h1><?php _e( 'Editor Settings', 'controlled-chaos-blocks' ); ?></h1>
 	<p class="description"><?php _e( 'Manage blocks in the WordPress editor.' ); ?></p>
 	<h2 class="nav-tab-wrapper">
-		<a href="?page=<?php echo CCB_ADMIN_SLUG; ?>&tab=block-use" class="nav-tab <?php echo $active_tab == 'block-use' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Block Use', 'controlled-chaos-blocks' ); ?></a>
+		<a href="?page=<?php echo CCB_ADMIN_SLUG; ?>&tab=block-usage" class="nav-tab <?php echo $active_tab == 'block-usage' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Block Usage', 'controlled-chaos-blocks' ); ?></a>
+		<a href="?page=<?php echo CCB_ADMIN_SLUG; ?>&tab=default-blocks" class="nav-tab <?php echo $active_tab == 'default-blocks' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Default Blocks', 'controlled-chaos-blocks' ); ?></a>
     </h2>
+	<form method="post" action="options.php">
+        <?php if ( $active_tab == 'block-usage' ) {
+            settings_fields( 'ccb_block_usage' );
+            do_settings_sections( 'ccb-block-usage' );
+			$save = __( 'Save Block Usage', 'controlled-chaos-blocks' );
+		} elseif ( $active_tab == 'default-blocks' ) {
+            settings_fields( 'ccb_block_usage' );
+            do_settings_sections( 'ccb-default-blocks' );
+			$save = __( 'Save Default Blocks', 'controlled-chaos-blocks' );
+		} ?>
+        <p class="submit"><?php submit_button( $save, 'primary', '', false, [] ); ?></p>
+    </form>
 </div>
